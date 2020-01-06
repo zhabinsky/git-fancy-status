f4ae51436fbe67a1c604c6ec5fd7bddaded8d751
Vladislav Zhabinsky <vladislavzhabinsky@gmail.com>
2020-01-06 04:13:38 +0200

    added time

--- src/git-status.sh
+++ src/git-status.sh
@@ -20,7 +20,7 @@ git_fancy_status() {
 		local home_path="/Users/${current_user}"
 		local path=$(pwd)
 		path=${path/"${home_path}"/'~'}
-		printf "📂  Path            : $(chlk $GREEN $path)\n"
+		printf "📂  Path                  : $(chlk $GREEN $path)\n"
 	}
 
 	log_commits() {
@@ -31,7 +31,7 @@ git_fancy_status() {
 		[[ -z "$my_commits" ]] && my_commits="1"
 		local my_commits_percent=$(bc -l <<<"scale=1; ${my_commits} * 100 /${commits}")
 		commits=${commits%.*}
-		echo "${1}: ${commits} (Mine: $(chlk $BLUE "~${my_commits_percent}%") ${my_commits})"
+		echo "${1}      : ${commits} (Mine: $(chlk $BLUE "~${my_commits_percent}%") ${my_commits})"
 	}
 
 	find_child_repos() {
@@ -48,7 +48,7 @@ git_fancy_status() {
 			fi
 			cd "$dirname"
 		done
-		[[ $foundGits = *[!\ ]* ]] && printf "❓  Git repos       :\n$foundGits\n\n"
+		[[ $foundGits = *[!\ ]* ]] && printf "❓  Git repos             :\n$foundGits\n\n"
 	}
 
 	echo
@@ -57,21 +57,28 @@ git_fancy_status() {
 		branch=$(printf "${GREEN}$(git branch | grep -e "*" | cut -c 3-)${RESET}")
 		repo_name=$(basename -s .git $(git config --get remote.origin.url))
 		say_path
+		local first_commit=$(git log --reverse | grep -e "^Date:  " | head -n 1 | cut -c 6- | xargs)
 
-		echo "🏠  Repository      : ${GREEN}${repo_name} ${RESET}(Contributors: ${BLUE}$(git shortlog --summary | wc -l | xargs)${RESET})"
-		echo "🐒  User            : ${GREEN}<${user_email}>${RESET} (${username})"
+		local commit_my_first=$(git log --reverse | grep "Vladislav" --a=1 | grep -e "^Date:  " | head -n 1 | cut -c 6- | xargs)
+		local commit_my_last=$(git log | grep "Vladislav" --a=1 | grep -e "^Date:  " | head -n 1 | cut -c 6- | xargs)
+
+		echo "🏠  Repository            : ${GREEN}${repo_name} ${RESET}(Contributors: ${BLUE}$(git shortlog --summary | wc -l | xargs)${RESET})"
+		echo "📌  Commit init           :${GREEN} ${first_commit}${RESET}"
+		echo "📌  Commit mine (first)   :${GREEN} ${commit_my_first}${RESET}"
+		echo "📌  Commit last (last)    :${GREEN} ${commit_my_last}${RESET}"
+		echo "🐒  User                  : ${GREEN}<${user_email}>${RESET} (${username})"
 
 		log_commits "🔥  Commits (Merge) " "--merges"
 		log_commits "🔥  Commits         " "--no-merges"
 
-		echo "📌  Branch          : ${branch} (Total: ${BLUE}$(git branch -a | wc -l | xargs))${RESET}"
-		echo "🛳️  HEAD is:        : ${BLUE}$(git log origin/master..HEAD | grep "^Author:" | wc -l | xargs)${RESET} Commits away from${GREEN} origin/master${RESET}"
+		echo "📌  Branch                : ${branch} (Total: ${BLUE}$(git branch -a | wc -l | xargs))${RESET}"
+		echo "🛳️  HEAD is:              : ${BLUE}$(git log origin/master..HEAD | grep "^Author:" | wc -l | xargs)${RESET} Commits away from${GREEN} origin/master${RESET}"
 
 		local status=$(git s --short)
 		status=${status:-"${SPACE}🐒   no-changes"}
 		printf "👌  Status... \n\n$(chlk $BLUE "$status")\n"
 	else
-		echo "⭕  Error           : $(chlk $RED "No .git repo found.")"
+		echo "⭕  Error                 : $(chlk $RED "No .git repo found.")"
 		say_path
 		find_child_repos
 	fi
